b3dc46b4978f4d42263c6eaeb358bd52f48f7ffc
zealleaf <feb.zealleaf@gmail.com>
2023-05-16 12:30:35 +0800

    chore: some change

--- lua/zealleaf/core/options.lua
+++ lua/zealleaf/core/options.lua
@@ -40,3 +40,5 @@ opt.splitright = true -- split vertical window to the right
 opt.splitbelow = true -- split horizontal window to the bottom
 
 opt.iskeyword:append("-") -- consider string-string as whole word
+
+opt.title = true
