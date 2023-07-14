9596a5aee4e884a8af6cf446210f8831a013a747
zealleaf <dev@zealleaf.me>
2023-07-14 16:39:37 +0800

    chore: some change

--- lua/zealleaf/plugins/lualine.lua
+++ lua/zealleaf/plugins/lualine.lua
@@ -3,7 +3,7 @@ return {
 	config = function()
 		require("lualine").setup({
 			options = {
-				theme = "ayu",
+				theme = "auto",
 				component_separators = "|",
 				section_separators = "",
 			},
