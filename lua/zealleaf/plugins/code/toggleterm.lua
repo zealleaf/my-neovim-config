4d041f24a97807a6b28eaf65b454dfc3f08d1ce3
zealleaf <dev@zealleaf.me>
2023-07-22 12:30:29 +0800

    chore: some change

--- lua/zealleaf/plugins/code/toggleterm.lua
+++ lua/zealleaf/plugins/code/toggleterm.lua
@@ -19,19 +19,12 @@ return {
 		vim.cmd("autocmd! TermOpen term://*toggleterm#* lua set_terminal_keymaps()")
 
 		toggleterm.setup({
-			size = function(term)
-				if term.direction == "horizontal" then
-					return 15
-				elseif term.direction == "vertical" then
-					return vim.o.columns * 0.3
-				end
-			end,
-			start_in_insert = true,
+			open_mapping = [[<leader>t]],
 		})
 
 		local Terminal = require("toggleterm.terminal").Terminal
 
-		local lazygit = Terminal:new({
+		local lazygit_term = Terminal:new({
 			cmd = "lazygit",
 			dir = "git_dir",
 			direction = "float",
@@ -54,64 +47,12 @@ return {
 			end,
 		})
 
-		local ta = Terminal:new({
-			direction = "float",
-			close_on_exit = true,
-		})
-
-		local tb = Terminal:new({
-			direction = "vertical",
-			close_on_exit = true,
-		})
-
-		local tc = Terminal:new({
-			direction = "horizontal",
-			close_on_exit = true,
-		})
-
-		local M = {}
-
-		M.toggleA = function()
-			if ta:is_open() then
-				ta:close()
-				return
-			end
-			tb:close()
-			tc:close()
-			ta:open()
+		---@diagnostic disable-next-line: lowercase-global
+		function lazygit_toggle()
+			lazygit_term:toggle()
 		end
 
-		M.toggleB = function()
-			if tb:is_open() then
-				tb:close()
-				return
-			end
-			ta:close()
-			tc:close()
-			tb:open()
-		end
-
-		M.toggleC = function()
-			if tc:is_open() then
-				tc:close()
-				return
-			end
-			ta:close()
-			tb:close()
-			tc:open()
-		end
-
-		M.toggleG = function()
-			lazygit:toggle()
-		end
-
-		local mapToggleTerm = function(tt)
-			vim.keymap.set({ "n", "t" }, "<leader>ta", tt.toggleA)
-			vim.keymap.set({ "n", "t" }, "<leader>ts", tt.toggleB)
-			vim.keymap.set({ "n", "t" }, "<leader>td", tt.toggleC)
-			vim.keymap.set({ "n", "t" }, "<leader>l", tt.toggleG)
-		end
-
-		mapToggleTerm(M)
+		vim.keymap.set({ "n", "t" }, "<leader>l", "<cmd>lua lazygit_toggle()<CR>")
+		vim.keymap.set({ "n", "t" }, "<leader>ta", "<cmd>ToggleTermToggleAll<CR>")
 	end,
 }
