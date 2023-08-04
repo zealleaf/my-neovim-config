09ab58b96d0f25e1c408f530ceb085fa828162fe
zealleaf <github@zealleaf.me>
2023-08-04 12:31:25 +0800

    chore: some change

--- lua/zealleaf/plugins/code/toggleterm.lua
+++ lua/zealleaf/plugins/code/toggleterm.lua
@@ -26,7 +26,7 @@ return {
 					return vim.o.columns * 0.3
 				end
 			end,
-			open_mapping = [[<leader>t]],
+			open_mapping = [[td]],
 		})
 
 		local Terminal = require("toggleterm.terminal").Terminal
@@ -106,10 +106,10 @@ return {
 		end
 
 		local mapToggleTerm = function(termMap)
-			vim.keymap.set({ "n", "t" }, "<leader>tf", termMap.toggle_float_term)
-			vim.keymap.set({ "n", "t" }, "<leader>tv", termMap.toggle_vertical_term)
-			vim.keymap.set({ "n", "t" }, "<leader>th", termMap.toggle_horizontal_term)
-			vim.keymap.set({ "n", "t" }, "<leader>g", termMap.toggle_lazygit_term)
+			vim.keymap.set({ "n", "t" }, "tf", termMap.toggle_float_term)
+			vim.keymap.set({ "n", "t" }, "tv", termMap.toggle_vertical_term)
+			vim.keymap.set({ "n", "t" }, "th", termMap.toggle_horizontal_term)
+			vim.keymap.set({ "n", "t" }, "tl", termMap.toggle_lazygit_term)
 		end
 
 		mapToggleTerm(term_map)
