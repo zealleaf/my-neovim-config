30f23b0c91fdf125a3d1d55471ab3406c18aa175
zealleaf <github@zealleaf.me>
2023-09-14 10:51:12 +0800

    chore: some change

--- lua/zealleaf/plugins/theme/nightfox.lua
+++ lua/zealleaf/plugins/theme/nightfox.lua
@@ -1,8 +1,8 @@
 return {
 	"EdenEast/nightfox.nvim",
 	lazy = false,
-	priority = 999,
+	priority = 1000,
 	config = function()
-		vim.cmd("colorscheme dayfox")
+		vim.cmd("colorscheme nightfox")
 	end,
 }
