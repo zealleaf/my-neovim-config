f3d0655d97b6d7d00e2a36d858700037077725f3
zealleaf <github@zealleaf.me>
2023-11-20 10:56:38 +0800

    chore: some change

--- lua/zealleaf/plugins/code/comment.lua
+++ lua/zealleaf/plugins/code/comment.lua
@@ -1,12 +1,8 @@
 return {
 	{
 		"numToStr/Comment.nvim",
-		dependencies = { "JoosepAlviste/nvim-ts-context-commentstring" },
 		event = "VeryLazy",
 		config = function()
-			vim.cmd("autocmd BufEnter * set formatoptions-=cro")
-			vim.cmd("autocmd BufEnter * setlocal formatoptions-=cro")
-
 			require("Comment").setup({
 				toggler = {
 					---Line-comment toggle keymap
@@ -20,7 +16,6 @@ return {
 					---Block-comment keymap
 					block = "<space>b",
 				},
-				pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
 			})
 		end,
 	},
